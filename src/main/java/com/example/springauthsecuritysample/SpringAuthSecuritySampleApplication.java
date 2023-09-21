5d74449902cae2b89ecf8d6c903f30b8b197f9fd
Lalu-Mahato <135944020+Lalu-Mahato@users.noreply.github.com>
2023-09-21 16:00:48 +0530

    Initial project setup

---
+++ src/main/java/com/example/springauthsecuritysample/SpringAuthSecuritySampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springauthsecuritysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthSecuritySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthSecuritySampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springauthsecuritysample/SpringAuthSecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springauthsecuritysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthSecuritySampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
